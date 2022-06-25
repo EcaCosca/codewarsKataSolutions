2d2577a38756cbbcf7c39b86e6b3dbd71d54817c
EcaCosca <enriquecoscarelli@gmail.com>
2022-06-25 11:02:25 -0300

    Done

--- 8kyu_Even or Odd.js
+++ 8kyu_Even or Odd.js
@@ -2,9 +2,7 @@
 
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-function even_or_odd(number) {
-  
-}
+const even_or_odd = n => n % 2 == 0 ? "Even" : "Odd"
 
 console.log(even_or_odd(2))
 console.log("Even")
