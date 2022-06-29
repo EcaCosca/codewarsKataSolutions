fb7d3c91dff90e95ca4924a887aec6162bab4407
EcaCosca <enriquecoscarelli@gmail.com>
2022-06-29 04:49:01 -0300

    Done

--- 7kyu_Debug Sum of Digits of a Number.js
+++ 7kyu_Debug Sum of Digits of a Number.js
@@ -7,11 +7,24 @@
 // 223  => 7
 // 1337 => 14
 
+// GIVEN
+// function getSumOfDigits(integer) {
+//     var sum = null;
+//     var digits =  Math.floor(integer).toString();
+//         for(var ix = 1; ix < digits.length; ix = sum + 1) {
+//             sum =+ digits[ix + 1]);
+//         }
+//     return sum;     
+// }
+
+// RESULT 
 function getSumOfDigits(integer) {
-    var sum = null;
-    var digits =  Math.floor(integer).toString();
-        for(var ix = 1; ix < digits.length; ix = sum + 1) {
-            sum =+ digits[ix + 1]);
-        }
+    let sum = 0;
+    let digits =  integer.toString().split('').map(Number);
+  console.log(digits)
+    
+    for(let ix = 0; ix < digits.length; ix++) {
+        sum += digits[ix];
+    }
     return sum;     
 }
