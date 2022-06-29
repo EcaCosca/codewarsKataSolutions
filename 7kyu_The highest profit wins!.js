d98c57a53d029097eea00e664e177dd3f2308291
EcaCosca <enriquecoscarelli@gmail.com>
2022-06-29 04:32:35 -0300

    ADDING SHORT VERSION

--- 7kyu_The highest profit wins!.js
+++ 7kyu_The highest profit wins!.js
@@ -13,9 +13,13 @@
 // Remarks
 // All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
 
+// LONG VERSION
 function minMax(arr){
     return [Math.min(...arr),Math.max(...arr)]; // fix me!
-  }
+}
+
+// SHOT VERSION
+const minMax = (arr) => [Math.min(...arr),Math.max(...arr)];
 
 console.log(minMax([1, 2, 3, 4, 5]))
 console.log([1, 5])
