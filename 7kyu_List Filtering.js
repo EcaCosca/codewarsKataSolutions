59b2eab16c223e75b820cfa38f23c8eb932b34d8
EcaCosca <enriquecoscarelli@gmail.com>
2022-07-02 13:11:48 -0300

    Setting up

--- 7kyu_List Filtering.js
+++ 7kyu_List Filtering.js
@@ -6,3 +6,10 @@
 // filter_list([1,2,'a','b']) == [1,2]
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+}
+
+console.log(filter_list([1,2,'a','b']))
+console.log([1,2])
