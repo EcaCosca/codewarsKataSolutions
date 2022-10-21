36d3e34b095a6f1ffc8c776df9d90d4b8d3ee7e6
EcaCosca <63668672+EcaCosca@users.noreply.github.com>
2022-10-21 19:32:16 +0200

    Create 7kyu_Vowel Count.js

---
+++ 7kyu_Vowel Count.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowelsList = 'aeiouAEIOU';
+  
+  for(var i = 0; i < str.length ; i++) {
+    if (vowelsList.indexOf(str[i]) !== -1)
+    {
+      vowelsCount += 1;
+    }
+  }
+  return vowelsCount;
+}
