fb7ed77bf4ed868cfc13bffe80b26ac95247a0b5
EcaCosca <63668672+EcaCosca@users.noreply.github.com>
2022-11-01 23:00:12 +0100

    Create 8kyu_Remove String Spaces.js

---
+++ 8kyu_Remove String Spaces.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+const noSpace = x => x.split(' ').join('')
